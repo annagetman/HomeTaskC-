1a07f9bb7a3f7e9dd7a629301b5143982764cca0
Ann <annaporik12@gmail.com>
2021-03-11 20:20:45 +0300

    add method in DoubleArray FindNumberOfArrayElementsThatAreLargerAllTheirNeighbors

--- HomeTaskApp/HomeTask/DoubleArray.cs
+++ HomeTaskApp/HomeTask/DoubleArray.cs
@@ -27,5 +27,31 @@ namespace HomeTask
             }
             return max;
         }
+
+
+        public static int FindNumberOfArrayElementsThatAreLargerAllTheirNeighbors(int[,] array)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if ((i <= 0 || array[i - 1, j] < array[i, j])
+                        && (i >= array.GetLength(0) - 1 || array[i + 1, j] < array[i, j])
+                        && (j <= 0 || array[i, j - 1] < array[i, j])
+                        && (j >= array.GetLength(1) - 1 || array[i, j + 1] < array[i, j]))
+                    {
+
+                        counter++;
+                    }
+                }
+            }
+            return counter;
+        }
+        
+
+
+
     }
 }
